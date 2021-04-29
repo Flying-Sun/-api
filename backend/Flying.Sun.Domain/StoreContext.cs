3526c04f72ea5176782d731020497b5a952986c6
datlaria <63686288+datlaria@users.noreply.github.com>
2021-04-28 22:50:44 -0400

    Store Context Changes

---
+++ backend/Flying.Sun.Domain/StoreContext.cs
@@ -0,0 +1,14 @@
+﻿using Flying.Sun.Domain.Catalog;
+using Microsoft.EntityFrameworkCore;
+
+namespace Flying.Sun.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options)
+            : base(options)
+        { }
+
+        public DbSet<Item> Items { get; set; }
+    }
+}
