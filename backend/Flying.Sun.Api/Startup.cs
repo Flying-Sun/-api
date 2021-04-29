d938757ab5cc87cfe2649962ba731b031bb332e5
datlaria <63686288+datlaria@users.noreply.github.com>
2021-04-28 23:47:48 -0400

    db changes

--- backend/Flying.Sun.Api/Startup.cs
+++ backend/Flying.Sun.Api/Startup.cs
@@ -26,6 +26,10 @@ namespace Flying.Sun.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<StoreContext>(opt ->
+            opt.UseSqlite(Configuration.GetConnectionString("LocalDb"),
+            b -> b.MigrationAssembly("Flying.Sun.Api")));
+
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
